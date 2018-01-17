a805bae710f269ed765ba4acbfea163b7e8725b1
d954mas <d954mas@gmail.com>
2018-01-17 13:51:47 +0300

    build work

--- native_raycasting/include/arrays.h
+++ native_raycasting/include/arrays.h
@@ -1,4 +1,7 @@
 #pragma once
+#include "texture.h"
 
 int ** createTwoArrayInt(int, int);
 int clearArrayInt(int **, int);
+Color ** createTwoArrayColor(int, int);
+void clearArrayColor(Color **, int);
--- native_raycasting/include/texture.h
+++ native_raycasting/include/texture.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <dmsdk/sdk.h>
 struct Color {
 	uint8_t r;
 	uint8_t g;
