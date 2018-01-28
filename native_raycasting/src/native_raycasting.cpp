426d2ccff3b5b60bfbc07db07a6354abed705fe8
Dima Popov <d954mas@gmail.com>
2018-01-28 19:19:02 +0300

    rename statics

--- native_raycasting/src/native_raycasting.cpp
+++ native_raycasting/src/native_raycasting.cpp
@@ -12,74 +12,74 @@
 #include "texture.h"
 #include "png.h"
 
-static Camera camera;
-static Buffer buffer;
-static Map map;
-static Plane plane;
-static double viewDist = 0;
-static std::vector<Texture> textures;
+static Camera CAMERA;
+static Buffer BUFFER;
+static Map MAP;
+static Plane PLANE;
+static std::vector<Texture> TEXTURES;
 static std::vector<double> PRE_CALC_HEIGHT_DISTANCE;
-static std::vector<double> distanceBuffer;
-static std::vector<Sprite> sprites;
-static std::vector<double> angles;
+static std::vector<double> DISTANCE_BUFFER;
+static std::vector<Sprite> SPRITES;
+static std::vector<double> ANGLES;
+static double viewDist = 0;
 
 static void viewDistanceUpdated(){
-	viewDist = plane.width/(2 * tan(camera.fov/2.0));
-	distanceBuffer.resize(plane.width);
-	PRE_CALC_HEIGHT_DISTANCE.resize(plane.width);
+	viewDist = PLANE.width/(2 * tan(CAMERA.fov/2.0));
+	DISTANCE_BUFFER.resize(PLANE.width);
+	PRE_CALC_HEIGHT_DISTANCE.resize(PLANE.width);
 	PRE_CALC_HEIGHT_DISTANCE.clear();
-	angles.resize(plane.halfWidth);
-	angles.clear();
-	for(int x=1;x<=plane.halfWidth;x++){
-		angles.push_back(atan(x/viewDist));
+	ANGLES.resize(PLANE.halfWidth);
+	ANGLES.clear();
+	for(int x=1;x<=PLANE.halfWidth;x++){
+		ANGLES.push_back(atan(x/viewDist));
 	}
-	double scale = viewDist/plane.height;
-	for(int i = 0;  i < plane.halfHeight; i++){
-		PRE_CALC_HEIGHT_DISTANCE.push_back(plane.halfHeight / (double)(plane.halfHeight - i) * scale);
+	double scale = viewDist/PLANE.height;
+	for(int i = 0;  i < PLANE.halfHeight; i++){
+		PRE_CALC_HEIGHT_DISTANCE.push_back(PLANE.halfHeight / (double)(PLANE.halfHeight - i) * scale);
 	}
 }
 
 void setCameraFov(double fov){
-	setCameraFov(&camera, fov);
+	setCameraFov(&CAMERA, fov);
 	viewDistanceUpdated();
 }
 
 void updatePlane(int x, int y, int endX, int endY){
-	updatePlane(&plane, x, y, endX, endY);
+	updatePlane(&PLANE, x, y, endX, endY);
 	viewDistanceUpdated();
 }
 
 void addSprite(double x, double y, int textureId){
 	Sprite sprite = {x,y,textureId,0};
-	sprites.push_back(sprite); 
+	SPRITES.push_back(sprite); 
 }
 
 void loadTexture(dmScript::LuaHBuffer* luaBuffer, int id){
-	if(id>=textures.size()){
-		textures.resize(id+1);
+	if(id>=TEXTURES.size()){
+		TEXTURES.resize(id+1);
 	}
-	decodeToTexture(luaBuffer->m_Buffer, &textures[id]);
+	decodeToTexture(luaBuffer->m_Buffer, &TEXTURES[id]);
 }
 
 void updateCamera(double x, double y, double angle){
-	updateCamera(&camera, x, y, angle);
+	updateCamera(&CAMERA, x, y, angle);
 }
 
 void setBuffer(int width, int height, dmScript::LuaHBuffer* luaBuffer){
-	initBuffer(&buffer, width, height, luaBuffer);
+	initBuffer(&BUFFER, width, height, luaBuffer);
 }
 
 void clearBuffer(){
-	clearBuffer(&buffer);
+	clearBuffer(&BUFFER);
 }
 
 void setMap(lua_State* L){
-	parseMap(L, &map);
+	parseMap(L, &MAP);
 }
 
 void castSingleRay(double rayAngle, double *perpDist, double *endX, 
 	double *endY, int *mapXResult, int *mapYResult, double *textureX){
-		castRay(&camera, map.walls, rayAngle, perpDist, endX, 
+		castRay(&CAMERA, MAP.walls, rayAngle, perpDist, endX, 
 			endY, mapXResult, mapYResult, textureX);
 }
 
@@ -92,16 +92,16 @@ static inline void countVertY(double dist, int* height, int* drawStart, int* dra
 	//int lineHeight = ((int)round(viewDist/dist)) & 0xFFFFFFFE; //must be even
 	*pixelYAdd = 1.0/(lineHeight-1);
 	*height = lineHeight;
-	if(lineHeight > plane.height){
+	if(lineHeight > PLANE.height){
 		int halfLineHeight = lineHeight>>1;
-		*pixelY = *pixelYAdd *  ((lineHeight - plane.height)>>1);
-		lineHeight = plane.height;
+		*pixelY = *pixelYAdd *  ((lineHeight - PLANE.height)>>1);
+		lineHeight = PLANE.height;
 	}else{
 		*pixelY = 0;
 	}
 	int halfLineHeight = lineHeight>>1;
-	*drawStart = plane.halfHeight - halfLineHeight;
-	*drawEnd =  plane.halfHeight + halfLineHeight - 1;
+	*drawStart = PLANE.halfHeight - halfLineHeight;
+	*drawEnd =  PLANE.halfHeight + halfLineHeight - 1;
 }
 
 static inline void drawVertLine(int x, int drawStart, int drawEnd,double pixelY, double pixelYAdd, Color** pixels, int pixelX){
@@ -109,7 +109,7 @@ static inline void drawVertLine(int x, int drawStart, int drawEnd,double pixelY,
 	Color* line = pixels[pixelX];
 	for (int y = drawStart; y < drawEnd; y++) {
 		//setPixel(&buffer, x, y, &pixels[(int)pixelY][pixelX]);
-		setPixel(&buffer, x, y, &line[(int)pixelY]);
+		setPixel(&BUFFER, x, y, &line[(int)pixelY]);
 		//bufferPixels[y][0] = line[(int)pixelY].colors[0];
 		//memcpy(&bufferPixels[y][0], &line[(int)pixelY].colors[0], 3);
 		//memcpy(&buffer.stream[(y * buffer.width + x) * 3], &line[(int)pixelY].colors[0], 3);
@@ -121,13 +121,13 @@ static inline void drawVertLine(int x, int drawStart, int drawEnd,double pixelY,
 static inline void drawVertLineTransparent(int x, int drawStart, int drawEnd,double pixelY, double pixelYAdd, Color** pixels, int pixelX){
 	Color* line = pixels[pixelX];
 	for (int y = drawStart; y < drawEnd; y++) {
-		setPixelTransparent(&buffer, x, y, &line[(int)pixelY]);
+		setPixelTransparent(&BUFFER, x, y, &line[(int)pixelY]);
 		pixelY += pixelYAdd;
 	}
 }
 
 static inline void drawWall(int x, int drawStart, int drawEnd, double pixelY, double pixelYAdd, double textureX, int textureId){
-	Texture* texture = &textures[textureId];
+	Texture* texture = &TEXTURES[textureId];
 	int pixelX = (int)( (texture->widthM)* textureX);
 	pixelY = pixelY * (texture->heightM);
 	pixelYAdd = pixelYAdd * (texture->heightM);
@@ -136,16 +136,16 @@ static inline void drawWall(int x, int drawStart, int drawEnd, double pixelY, do
 
 static inline void drawSprites(){
 	//draw sprites
-	double cameraAngle = TWO_PI - (camera.angle - HALF_PI); 
+	double cameraAngle = TWO_PI - (CAMERA.angle - HALF_PI); 
 	//printf("cameraAngle:%f\n",cameraAngle);
 	double cameraCos = cos(cameraAngle);
 	double cameraSin = sin(cameraAngle);
-	for (int i = 0; i < sprites.size(); i++)
+	for (int i = 0; i < SPRITES.size(); i++)
 	{
-		Sprite* sprite = &sprites[i];
+		Sprite* sprite = &SPRITES[i];
 		//translate
-		double dx = sprite->x - camera.x;
-		double dy = sprite->y - camera.y;
+		double dx = sprite->x - CAMERA.x;
+		double dy = sprite->y - CAMERA.y;
 		//printf("dx:%f dy:%f\n",dx,dy);
 		//rotate
 		double rotatedDx =- dx * cameraSin + dy * cameraCos;
@@ -153,12 +153,12 @@ static inline void drawSprites(){
 		sprite->dx = rotatedDx;
 		sprite->dy = rotatedDy;
 	}
-	std::sort(sprites.begin(), sprites.end(), sortSprites);
+	std::sort(SPRITES.begin(), SPRITES.end(), sortSprites);
 
 	//printf("cameraAngle:%f", cameraAngle);
-	for (int i = 0; i < sprites.size(); i++)
+	for (int i = 0; i < SPRITES.size(); i++)
 	{
-		Sprite* sprite = &sprites[i];
+		Sprite* sprite = &SPRITES[i];
 		//printf("rotated x:%f y:%f\n",sprite->dx,sprite->dy);
 		if(sprite->dy<=0){
 			continue;
@@ -167,12 +167,12 @@ static inline void drawSprites(){
 		int lineHeight, drawStart,drawEnd;
 		double pixelY, pixelYAdd;
 		countVertY(sprite->dy,&lineHeight,&drawStart,&drawEnd,&pixelY, &pixelYAdd);
-		Texture* texture = &textures[sprite->textureId];
+		Texture* texture = &TEXTURES[sprite->textureId];
 		pixelY = pixelY * (texture->heightM);
 		pixelYAdd = pixelYAdd * (texture->heightM);
 		int halfLineHeight = lineHeight>>1;
 		//sprite->dx += camera.fov/2.0;
-		int centerX = plane.halfWidth - (sprite->dx*lineHeightDouble);
+		int centerX = PLANE.halfWidth - (sprite->dx*lineHeightDouble);
 		int startX = centerX - halfLineHeight;
 		int endX = centerX + halfLineHeight;
 		// printf("centerX:%d startX:%d endX:%d\n ",centerX, startX, endX);
@@ -184,12 +184,12 @@ static inline void drawSprites(){
 			pixelX = addX * -startX;
 			startX = 0;
 		}	
-		if(endX > plane.endX-1){
-			endX = plane.endX-1;
+		if(endX > PLANE.endX-1){
+			endX = PLANE.endX-1;
 		}
 		//printf("pixelY%f addY:%f\n",pixelY,pixelYAdd);
 		for(int x = startX; x<endX; x++){
-			if(sprite->dy <=distanceBuffer[x]){
+			if(sprite->dy <=DISTANCE_BUFFER[x]){
 				drawVertLineTransparent(x, drawStart, drawEnd, pixelY, pixelYAdd, texture->pixels, (int)pixelX);
 				//printf("pixelX:%f\n",pixelX);
 			}
@@ -205,47 +205,47 @@ static void inline drawFloorsAndCeilings(int x, int drawStart, double perpDist, 
 		double weight = currentDist/perpDist;
 		double weight2 = 1.0 - weight;
 		//printf("weight:%f\n", weight);
-		double floorX = (weight * endX + weight2 * camera.x);
-		double floorY = (weight * endY + weight2 * camera.y);
+		double floorX = (weight * endX + weight2 * CAMERA.x);
+		double floorY = (weight * endY + weight2 * CAMERA.y);
 		//	printf("floorX:%f floorY:%f\n", floorX, floorY);
 		int cellX = (int)(floorX);
 		int cellY = (int)(floorY);
-		int floorId = map.floors[cellY][cellX];
-		int ceilId = map.ceils[cellY][cellX];
+		int floorId = MAP.floors[cellY][cellX];
+		int ceilId = MAP.ceils[cellY][cellX];
 		//printf("floor id:%d\n", floorId);
-		Texture* floorTexture = &textures[floorId];
+		Texture* floorTexture = &TEXTURES[floorId];
 		int textureX = (floorX - (int)floorX) * floorTexture->widthM;
 		int textureY = (floorY - (int)floorY) * floorTexture->heightM;
 		//		printf("textureX:%d textureY:%d\n", textureX, textureY);
 		Color *color = &(floorTexture->pixels[textureX][textureY]);
 		//	printf("draw4\n");
 		//	printf("x:%d y:%d\n", x, y);
-		setPixel(&buffer, x, y, color);
+		setPixel(&BUFFER, x, y, color);
 		//	printf("draw5\n");
-		floorTexture = &textures[ceilId];
+		floorTexture = &TEXTURES[ceilId];
 		color = &(floorTexture->pixels[textureX][textureY]);
-		setPixel(&buffer, x, plane.height - y-1, color);
+		setPixel(&BUFFER, x, PLANE.height - y-1, color);
 	}
 }
 
 void castRays(){
 	double perpDist, endX, endY, textureX;
 	int mapX, mapY;
-	int** floors =  map.floors;
-	int** ceils =  map.ceils;
+	int** floors =  MAP.floors;
+	int** ceils =  MAP.ceils;
 	int lineHeight, drawStart,drawEnd;
 	double pixelY, pixelYAdd;
-	for(int i=0; i<plane.halfWidth; i++){
-		double rayAngle = angles[i];
+	for(int i=0; i<PLANE.halfWidth; i++){
+		double rayAngle = ANGLES[i];
 		for(int j=0;j<2;j++){
 			double currentAngle = (j==1) ? -rayAngle : rayAngle;
-			int x = (j==1) ? (plane.halfWidth) - i-1 : (plane.halfWidth) + i;
+			int x = (j==1) ? (PLANE.halfWidth) - i-1 : (PLANE.halfWidth) + i;
 			//cast ray
 			castSingleRay(currentAngle, &perpDist, &endX, &endY, &mapX, &mapY, &textureX);
-			distanceBuffer[x] =  perpDist;
+			DISTANCE_BUFFER[x] =  perpDist;
 			//draw wall
 			countVertY(perpDist,&lineHeight,&drawStart,&drawEnd,&pixelY, &pixelYAdd);
-			drawWall(x,drawStart, drawEnd, pixelY, pixelYAdd, textureX, map.walls[mapY][mapX]);
+			drawWall(x,drawStart, drawEnd, pixelY, pixelYAdd, textureX, MAP.walls[mapY][mapX]);
 			drawFloorsAndCeilings(x, drawStart, perpDist, endX, endY);
 		}
 	}
